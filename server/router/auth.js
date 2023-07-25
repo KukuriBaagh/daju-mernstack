db9084e3997e5e07fbe6a32a9c20a344de18b449
KukuriBaagh <howtoccdemo@gmail.com>
2023-07-25 10:30:47 +0530

    fetching data - initial commit

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../model/userSchema');
+
+
 // hompage
 router.get('/', (req, res) => {
     res.send("Hello From Router js");
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({ msg: req.body })
+
+    const { name, email, phone, work, password, cpassword } = req.body ;
+
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "Please Fill the form Properly...?"});
+    }
+
+    User.findOne({ email: email})
+    .then((userExist) => {
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exist !" });
+        }
+
+        const user = new User({name, email, phone, work, password, cpassword});
+
+        user.save().then(() => {
+            res.status(201).json({ msg: "user regsitered successfully." })
+        }).catch((err) => {
+            res.status(500).json({ error: "Failed to register !" })
+        })
+
+    }).catch((err) => {
+        res.status(500).json({ msg: "email verification failed !" })
+    })
+
 });
 
 module.exports = router;
