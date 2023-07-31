0e5a6d067c98827cc9e38410da176268a53cf4dc
KukuriBaagh <howtoccdemo@gmail.com>
2023-07-31 21:18:29 +0530

    signin validation succesfull

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,10 +4,15 @@ const DB = process.env.MONGO_URI;
 
 mongoose
   .connect(DB, {
+    socketTimeoutMS: 1000,
+    w: "majority",
+    authSource: "admin",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(`Connected to MongoDB`);
+    console.log(`Connected to daju-mernstack`);
   })
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error(err);
+  });
