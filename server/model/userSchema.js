c70fb47349c1c995abd39f28b7134c5c397e24cd
KukuriBaagh <howtoccdemo@gmail.com>
2023-08-01 13:15:20 +0530

    json web token implemented

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,6 +27,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  tokens: {
+    token: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
 userSchema.pre("save", async function (next) {
@@ -36,6 +43,18 @@ userSchema.pre("save", async function (next) {
   }
   next();
 });
+
+// user token handle
+userSchema.method.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concate({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const User = mongoose.model("USER", userSchema);
 
